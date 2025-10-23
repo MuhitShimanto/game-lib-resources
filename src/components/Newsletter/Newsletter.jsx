22c35b82b9f7cbe837aa6b0b0e77edb71b493c89
MuhitShimanto <muhit.shimanto02@gmail.com>
2025-10-23 16:08:34 +0600

    Responsive Newsletter

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -15,23 +15,23 @@ const Newsletter = () => {
             saturation={0.15}
             hueShift={240}
           />
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[1600px] mx-auto flex justify-between items-center">
-            <div className="w-full flex flex-col justify-center gap-12">
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[1600px] mx-auto flex flex-col lg:flex-row justify-between items-center">
+            <div className="w-full flex flex-col items-center lg:items-baseline justify-center gap-12">
               <h1 className="font-bold text-5xl montserrat">Stay up to date</h1>
-              <p className="text-secondary-content/65 poppins">
+              <p className="text-secondary-content/65 poppins text-center lg:text-left">
                 Get the latest on indie games, free game keys, developer
                 spotlights, and exclusive deals delivered straight to you. No
                 spam, just games.
               </p>
             </div>
             <div className="w-full flex justify-center items-center">
-              <form className="w-1/2 flex">
+              <form className="mt-6 lg:mt-0 w-3/4 lg:w-1/2 flex-col lg:flex-row">
                 <input
                   type="email"
                   className="input input-bordered w-full outline-none hover:border-primary focus:border-primary focus:shadow-sm focus:shadow-primary transition ease-in-out duration-200 cursor-target text-lg py-2 px-4"
                   placeholder="Enter your email"
                 />
-                <button className="py-2 px-4 bg-primary text-primary-content font-medium rounded-tr rounded-br cursor-pointer">
+                <button className="mt-3 lg:mt-0 py-2 px-4 bg-primary text-primary-content font-medium lg:rounded-tr lg:rounded-br cursor-pointer">
                   Subscribe
                 </button>
               </form>
